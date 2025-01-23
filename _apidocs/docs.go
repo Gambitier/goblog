4b60edea6a8fb151c2fffe497dc4b7a504653b6c
Gambitier <akash.jadhav.cse@gmail.com>
2025-01-23 21:53:38 +0530

    chore: remove hardcoded host configuration from API documentation files

--- _apidocs/docs.go
+++ _apidocs/docs.go
@@ -351,7 +351,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:3000",
+	Host:             "",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Go Blog API",
