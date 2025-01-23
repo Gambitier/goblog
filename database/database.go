2e9078b495dbefed76e33bf5bae4918980ba7bb2
Gambitier <akash.jadhav.cse@gmail.com>
2025-01-23 21:14:20 +0530

    refactor: update database connection string format to use PostgreSQL URI scheme

--- database/database.go
+++ database/database.go
@@ -16,8 +16,8 @@ type Database struct {
 
 func NewDatabase(cfg *config.DBConfig) (*Database, error) {
 	connStr := fmt.Sprintf(
-		"host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
-		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.DBName, cfg.SSLMode,
+		"postgresql://%s:%s@%s:%d/%s?sslmode=%s",
+		cfg.User, cfg.Password, cfg.Host, cfg.Port, cfg.DBName, cfg.SSLMode,
 	)
 
 	db, err := sql.Open("postgres", connStr)
